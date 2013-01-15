9c380632e25c3bf9510e7fe979d04212261266ff
Ryan Beverly <rbeverly@mymail.mines.edu>
2013-01-14 18:53:03 -0700

    full implementation on firefox

--- project01/functions.js
+++ project01/functions.js
@@ -1,3 +1,17 @@
-function colorchange(num) {
-	
+function colorchange(num, id) {
+	var inv = document.getElementById(id);
+	var style = document.defaultView.getComputedStyle(inv, null);
+	if (num == 0) {
+		inv.style.color = "blue";
+	}
+	else {
+		inv.style.color = "red";
+	}
+}
+
+function check(id) {
+	var text = document.getElementById(id).value;
+	if (text==null || text=="") {
+		alert("Surely you must feel something?");
+	}
 }
